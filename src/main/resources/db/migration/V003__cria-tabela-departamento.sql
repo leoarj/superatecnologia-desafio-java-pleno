cf25db36d3a1617590ee806a52c5290a2ef3744d
Leandro Antônio Rocha Araújo <39884377+leoarj@users.noreply.github.com>
2025-11-20 20:40:35 -0400

    feat(db): Adiciona migration para criar tabela departamento

---
+++ src/main/resources/db/migration/V003__cria-tabela-departamento.sql
@@ -0,0 +1,17 @@
+-- Departamento (Semelhante a ROLE ou Grupo de Acesso nesse caso)
+
+CREATE TABLE public.departamento
+(
+    id bigserial NOT NULL,
+    nome character varying(60) NOT NULL,
+    descricao character varying(255),
+    ativo boolean,
+    CONSTRAINT pk_departamento PRIMARY KEY (id),
+    CONSTRAINT un_departamento_nome UNIQUE (nome)
+);
+
+COMMENT ON TABLE public.departamento
+  IS 'Tabela para registro de departamentos disponíveis associados aos usuários.';
+
+ALTER TABLE IF EXISTS public.departamento
+    OWNER to postgres;
