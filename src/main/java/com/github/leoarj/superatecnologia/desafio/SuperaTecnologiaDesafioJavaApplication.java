9d3a911ac8f370a33759280521e1ad5512b4a8c0
Leandro Antônio Rocha Araújo <39884377+leoarj@users.noreply.github.com>
2025-11-20 20:08:55 -0400

    feat: Adiciona versão estrutura inicial do projeto
    Adiciona estrutura inicial do projeto (pom.xml) e dependências
    principais.

---
+++ src/main/java/com/github/leoarj/superatecnologia/desafio/SuperaTecnologiaDesafioJavaApplication.java
@@ -0,0 +1,13 @@
+package com.github.leoarj.superatecnologia.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperaTecnologiaDesafioJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperaTecnologiaDesafioJavaApplication.class, args);
+	}
+
+}
