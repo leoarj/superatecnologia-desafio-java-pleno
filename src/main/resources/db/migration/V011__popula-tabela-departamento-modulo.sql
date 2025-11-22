5629edd9fd95ab48cbe71afc8ade5038a1126c09
Leandro Antônio Rocha Araújo <39884377+leoarj@users.noreply.github.com>
2025-11-21 21:00:43 -0400

    feat(db): Adiciona migration para popular tabela departamento_modulo
    Adiciona migration para popular tabela de compatibilidade/concessão
    entre Departamentos e Módulos.

---
+++ src/main/resources/db/migration/V011__popula-tabela-departamento-modulo.sql
@@ -0,0 +1,19 @@
+-- Vincula Departamentos aos Módulos permitidos
+
+INSERT INTO public.departamento_modulo (departamento_id, modulo_id) VALUES
+-- 1. TI (Todos os módulos: 1 a 10)
+(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7), (1, 8), (1, 9), (1, 10),
+
+-- 2. Financeiro (Portal, Relatórios, Gestão Fin., Aprovador Fin., Solicitante Fin.)
+(2, 1), (2, 2), (2, 3), (2, 4), (2, 5),
+
+-- 3. RH (Portal, Relatórios, Admin RH, Colaborador RH)
+(3, 1), (3, 2), (3, 6), (3, 7),
+
+-- 4. Operações (Portal, Relatórios, Estoque, Compras)
+(4, 1), (4, 2), (4, 8), (4, 9),
+
+-- 5. Outros (Portal, Relatórios)
+(5, 1), (5, 2)
+
+ON CONFLICT (departamento_id, modulo_id) DO NOTHING;
