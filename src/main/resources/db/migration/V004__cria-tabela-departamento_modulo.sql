ea511f4b6d19a470c558bca99a486649b3016ee8
Leandro Antônio Rocha Araújo <39884377+leoarj@users.noreply.github.com>
2025-11-21 20:57:41 -0400

    refactor(db): Renomeia tabela de asosciação entre Departamentos e Módulos
    Renomeia tabela para dar melhor sentido semântico (Departamento
    usa/possui Módulo).

---
+++ src/main/resources/db/migration/V004__cria-tabela-departamento_modulo.sql
@@ -0,0 +1,19 @@
+-- Relação entre Módulos e Departamentos (Compatibilidade)
+
+CREATE TABLE public.departamento_modulo
+(
+    departamento_id bigint NOT NULL,
+    modulo_id bigint NOT NULL,
+
+    CONSTRAINT pk_departamento_modulo PRIMARY KEY (departamento_id, modulo_id),
+    CONSTRAINT fk_departamento_modulo_departamento FOREIGN KEY (departamento_id)
+        REFERENCES public.departamento (id) ON DELETE CASCADE,
+    CONSTRAINT fk_departamento_modulo_modulo FOREIGN KEY (modulo_id)
+            REFERENCES public.modulo (id) ON DELETE CASCADE
+);
+
+COMMENT ON TABLE public.departamento_modulo
+  IS 'Tabela para registro de compatibilidade entre Departamentos e Módulos.';
+
+ALTER TABLE IF EXISTS public.departamento_modulo
+    OWNER to postgres;
