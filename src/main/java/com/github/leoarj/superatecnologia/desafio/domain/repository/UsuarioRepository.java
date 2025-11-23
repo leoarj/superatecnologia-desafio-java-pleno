c482e4eb11dd8bd75a9073f7af7ec38bd7a2aeb2
Leandro Antônio Rocha Araújo <39884377+leoarj@users.noreply.github.com>
2025-11-23 17:20:51 -0400

    perf(domain/repository): Adiciona consulta customizada otimização na busca de Usuário
    - Adiciona consulta customizada devido o fato de que os departamentos e
    módulos devem vir na mesma consula (usos nos testes e otimização).

--- src/main/java/com/github/leoarj/superatecnologia/desafio/domain/repository/UsuarioRepository.java
+++ src/main/java/com/github/leoarj/superatecnologia/desafio/domain/repository/UsuarioRepository.java
@@ -2,6 +2,7 @@ package com.github.leoarj.superatecnologia.desafio.domain.repository;
 
 import com.github.leoarj.superatecnologia.desafio.domain.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -14,4 +15,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     // Opcional: query para buscar usuário já com os relacionamentos carregados (evitar N+1)
     // @Query("from Usuario u join fetch u.departamento left join fetch u.modulosConcedidos where u.email = :email")
     // Optional<Usuario> findByEmailComRelacionamentos OuComFetch(String email);
+
+    @Query("from Usuario u join fetch u.departamento left join fetch u.modulosConcedidos where u.id = :id")
+    Optional<Usuario> findByIdComRelacionamentos(Long id);
 }
