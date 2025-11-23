c6d5c78fc062601101db051afc313e03ffbac204
Leandro Antônio Rocha Araújo <39884377+leoarj@users.noreply.github.com>
2025-11-22 20:33:14 -0400

    feat(controller): Adiciona ApiExceptionHandler
    - Adiciona ApiExceptionHandler para captura/tratamento global de erros
    da API.
    - Adiciona apenas o método para tratar retorno HTTP para registros não
    localizados (demais tratamentos não adicionados nesse commit para não
    afetar a análise de cobertura da JaCoCo).

---
+++ src/main/java/com/github/leoarj/superatecnologia/desafio/api/ApiExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.github.leoarj.superatecnologia.desafio.api;
+
+import com.github.leoarj.superatecnologia.desafio.domain.exception.EntidadeNaoEncontradaException;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.URI;
+
+/*
+* Ponto único para tratamento de exceções de forma global.
+*
+* ResponseEntityExceptionHandler = estende essa classe que por padrão
+* já trata as mensagens de erro de acordo com a RFC 7807.
+* Ref.: https://www.rfc-editor.org/rfc/rfc7807
+*/
+
+@AllArgsConstructor
+@RestControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EntidadeNaoEncontradaException.class)
+    public ProblemDetail handleEntidadeNaoEncontrada(EntidadeNaoEncontradaException e) {
+        //return ResponseEntity.badRequest().body(e.getMessage());
+        ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND); //404
+        problemDetail.setTitle(e.getMessage());
+        problemDetail.setType(URI.create("https://github.leoarj.superatecnologia.desafio.com/erros/nao-encontrado"));
+
+        return problemDetail;
+    }
+}
