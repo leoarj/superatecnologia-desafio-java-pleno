576186194eb383a6c88e4162794dc1d46e9e3612
Leandro Antônio Rocha Araújo <39884377+leoarj@users.noreply.github.com>
2025-11-22 20:29:03 -0400

    feat(controller/dto): Adiciona modelo de representação resumido para Departamento
    - Adiciona o modelo de representação resumido para Departamento,
    utilizado na listagem geral de módulos.

---
+++ src/main/java/com/github/leoarj/superatecnologia/desafio/api/v1/model/DepartamentoResumoResponse.java
@@ -0,0 +1,13 @@
+package com.github.leoarj.superatecnologia.desafio.api.v1.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DepartamentoResumoResponse {
+
+    private Long id;
+    private String nome;
+
+}
