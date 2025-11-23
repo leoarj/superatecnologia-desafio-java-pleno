7ae207ae31e454aede441ba6cb783f19227f8d48
Leandro Antônio Rocha Araújo <39884377+leoarj@users.noreply.github.com>
2025-11-23 17:32:21 -0400

    refactor(unit-test): Refatora testes unitários de Usuario para usar Instancio

--- src/test/java/com/github/leoarj/superatecnologia/desafio/domain/service/CadastroUsuarioServiceTest.java
+++ src/test/java/com/github/leoarj/superatecnologia/desafio/domain/service/CadastroUsuarioServiceTest.java
@@ -3,6 +3,7 @@ package com.github.leoarj.superatecnologia.desafio.domain.service;
 import com.github.leoarj.superatecnologia.desafio.domain.exception.UsuarioNaoEncontradoException;
 import com.github.leoarj.superatecnologia.desafio.domain.model.Usuario;
 import com.github.leoarj.superatecnologia.desafio.domain.repository.UsuarioRepository;
+import org.instancio.Instancio;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,6 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.instancio.Select.field;
 
 @ExtendWith(MockitoExtension.class)
 class CadastroUsuarioServiceTest {
@@ -27,52 +29,58 @@ class CadastroUsuarioServiceTest {
 
     @Test
     void listar_DeveRetornarTodosUsuarios_QuandoExistirem() {
-        // 1. Arrange
-        Usuario u1 = new Usuario();
-        u1.setId(1L);
-        u1.setNome("João");
+        // Arrange
+        // Gera usuário 1 com ID e Nome fixos
+        Usuario u1 = Instancio.of(Usuario.class)
+                .set(field(Usuario::getId), 1L)
+                .set(field(Usuario::getNome), "João")
+                .create();
 
-        Usuario u2 = new Usuario();
-        u2.setId(2L);
-        u2.setNome("Maria");
+        // Gera usuário 2 com ID e Nome fixos
+        Usuario u2 = Instancio.of(Usuario.class)
+                .set(field(Usuario::getId), 2L)
+                .set(field(Usuario::getNome), "Maria")
+                .create();
 
         Mockito.when(usuarioRepository.findAll()).thenReturn(List.of(u1, u2));
 
-        // 2. Act
+        // Act
         List<Usuario> resultado = cadastroUsuarioService.listar();
 
-        // 3. Assert
+        // Assert
         assertThat(resultado).hasSize(2);
         assertThat(resultado).contains(u1, u2);
     }
 
     @Test
     void buscarOuFalhar_DeveRetornarUsuario_QuandoIdExistir() {
-        // 1. Arrange
+        // Arrange
         Long usuarioId = 1L;
-        Usuario usuarioMock = new Usuario();
-        usuarioMock.setId(usuarioId);
-        usuarioMock.setNome("Teste");
+
+        // Precisa somente do ID fixado
+        Usuario usuarioMock = Instancio.of(Usuario.class)
+                .set(field(Usuario::getId), usuarioId)
+                .create();
 
         Mockito.when(usuarioRepository.findById(usuarioId))
                 .thenReturn(Optional.of(usuarioMock));
 
-        // 2. Act
+        // Act
         Usuario resultado = cadastroUsuarioService.buscarOuFalhar(usuarioId);
 
-        // 3. Assert
+        // Assert
         assertThat(resultado).isNotNull();
         assertThat(resultado.getId()).isEqualTo(usuarioId);
     }
 
     @Test
     void buscarOuFalhar_DeveLancarException_QuandoIdNaoExistir() {
-        // 1. Arrange
+        // Arrange
         Long usuarioId = 999L;
         Mockito.when(usuarioRepository.findById(usuarioId))
                 .thenReturn(Optional.empty());
 
-        // 2 & 3. Act & Assert
+        // Act & Assert
         Assertions.assertThrows(UsuarioNaoEncontradoException.class, () -> {
             cadastroUsuarioService.buscarOuFalhar(usuarioId);
         });
