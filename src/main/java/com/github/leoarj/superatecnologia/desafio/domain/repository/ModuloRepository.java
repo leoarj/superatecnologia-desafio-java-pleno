35961cdff8f9a0b3e22da83b6f2737f585d9b017
Leandro Antônio Rocha Araújo <39884377+leoarj@users.noreply.github.com>
2025-11-23 17:18:25 -0400

    perf(domain/repository): Adiciona consulta customizada otimização na listagem dos Módulos (Individual)

--- src/main/java/com/github/leoarj/superatecnologia/desafio/domain/repository/ModuloRepository.java
+++ src/main/java/com/github/leoarj/superatecnologia/desafio/domain/repository/ModuloRepository.java
@@ -6,14 +6,18 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ModuloRepository extends JpaRepository<Modulo, Long> {
 
     /***
      * findAll() customizado com query para evitar N+1,
-     * já que pelos requisitos todos os dados devem ser trazidos a listagem no collection resource (listagem geral).
+     * já que pelos requisitos todos os dados devem ser trazidos na listagem no collection resource (listagem geral).
      * */
     @Query("select distinct m from Modulo m left join fetch m.departamentosPermitidos left join fetch m.modulosIncompativeis")
-    List<Modulo> findAll();
+    List<Modulo> findAllComRelacionamentos();
+
+    @Query("from Modulo m left join fetch m.departamentosPermitidos left join fetch m.modulosIncompativeis where m.id = :id")
+    Optional<Modulo> findByIdComRelacionamentos(Long id);
 }
