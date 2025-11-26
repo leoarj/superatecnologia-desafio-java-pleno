5e57ce5ab6380b9df5dd58decbdca3b2eae74d3f
Leandro Antônio Rocha Araújo <39884377+leoarj@users.noreply.github.com>
2025-11-26 19:30:48 -0400

    feat(integration-test): Adiciona classes de testes de integração
    - Adiciona teste unitário para FluxoSolicitacaoController.
    - Adiciona teste para ModuloController.
    - Adiciona teste para AutenticacaoController.

--- src/test/java/com/github/leoarj/superatecnologia/desafio/api/v1/controller/ModuloControllerIT.java
+++ src/test/java/com/github/leoarj/superatecnologia/desafio/api/v1/controller/ModuloControllerIT.java
@@ -1,9 +1,13 @@
 package com.github.leoarj.superatecnologia.desafio.api.v1.controller;
 
+import com.github.leoarj.superatecnologia.desafio.domain.model.Usuario;
+import com.github.leoarj.superatecnologia.desafio.domain.repository.UsuarioRepository;
+import com.github.leoarj.superatecnologia.desafio.domain.service.TokenService;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -14,24 +18,39 @@ import static org.hamcrest.Matchers.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test") // Força o uso do H2 e migrations H2
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+//@AutoConfigureMockMvc
 class ModuloControllerIT {
 
     @LocalServerPort
     private int port;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private TokenService tokenService;
+
+    private String tokenAdmin; //
 
     @BeforeEach
     void setUp() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = port;
         RestAssured.basePath = "/v1/modulos";
+
+        Usuario usuario = usuarioRepository.findById(1L).orElseThrow();
+        tokenAdmin = tokenService.gerarToken(usuario);
     }
 
+    //@WithMockUsuario(id = 1L) // <--- Simula o Usuário 1 logado
     @Test
     void listar_DeveRetornarStatus200_E_ListaDeModulos() {
         // Como o Flyway roda automaticamente antes dos testes iniciarem,
         // o banco H2 já tem os dados inseridos pelo V008.
 
         given()
+                .header("Authorization", "Bearer " + tokenAdmin) // <--- Passamos o Token
                 .accept(ContentType.JSON)
                 .when()
                 .get()
@@ -41,12 +60,14 @@ class ModuloControllerIT {
                 .body("nome", hasItems("Portal do Colaborador", "Gestão Financeira"));
     }
 
+    //@WithMockUsuario(id = 1L) // <--- Simula o Usuário 1 logado
     @Test
     void buscar_DeveRetornarModuloCompleto_QuandoIdExistir() {
         // Teste: Módulo 4 (Aprovador Financeiro) que tem relacionamento
         Long moduloId = 4L;
 
         given()
+                .header("Authorization", "Bearer " + tokenAdmin) // <--- Passamos o Token
                 .pathParam("moduloId", moduloId)
                 .accept(ContentType.JSON)
                 .when()
@@ -61,9 +82,11 @@ class ModuloControllerIT {
                 .body("modulosIncompativeis[0].id", equalTo(5));
     }
 
+    //@WithMockUsuario(id = 1L) // <--- Simula o Usuário 1 logado
     @Test
     void buscar_DeveRetornarStatus404_QuandoIdInexistente() {
         given()
+                .header("Authorization", "Bearer " + tokenAdmin) // <--- Passamos o Token
                 .pathParam("moduloId", 999)
                 .accept(ContentType.JSON)
                 .when()
