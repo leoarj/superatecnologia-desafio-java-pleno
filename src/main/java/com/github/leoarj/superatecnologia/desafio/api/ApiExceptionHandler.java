a414e6adcfbfa993e8bd39115262abfb77b26bd7
Leandro Antônio Rocha Araújo <39884377+leoarj@users.noreply.github.com>
2025-11-26 19:26:31 -0400

    feat(api): Adiciona ApiExceptionHandler

--- src/main/java/com/github/leoarj/superatecnologia/desafio/api/ApiExceptionHandler.java
+++ src/main/java/com/github/leoarj/superatecnologia/desafio/api/ApiExceptionHandler.java
@@ -3,13 +3,20 @@ package com.github.leoarj.superatecnologia.desafio.api;
 import com.github.leoarj.superatecnologia.desafio.domain.exception.EntidadeNaoEncontradaException;
 import com.github.leoarj.superatecnologia.desafio.domain.exception.NegocioException;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.*;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.net.URI;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
 * Ponto único para tratamento de exceções de forma global.
@@ -22,6 +29,41 @@ import java.net.URI;
 @AllArgsConstructor
 @RestControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    // Para ter acesso ao recurso de mensagens personalizadas de validação (messages.properties).
+    private final MessageSource messageSource;
+
+    /*
+     * Método para personalizar a resposta de acordo com a RFC 7807,
+     * para a exceção de MethodArgumentNotValidException.
+     */
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatus(status);
+        problemDetail.setTitle("Um ou mais campos estão inválidos");
+        problemDetail.setType(URI.create("https://algatransito.com/erros/campos-invalidos"));
+
+        // Extrai da exceção um mapa com os campos associados a suas respectivas mensagens de erro.
+        // Com MessageSource: Recupera a mensagem específica de acordo com o code do objeto de erro.
+        Map<String, String> fields = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .collect(Collectors.toMap(objectError -> ((FieldError) objectError).getField(),
+                        //DefaultMessageSourceResolvable::getDefaultMessage));
+                        objectError -> messageSource.getMessage(objectError, LocaleContextHolder.getLocale())));
+
+        // Cria uma propriedade personalizada no corpo do detalhamento de erro
+        // passando o mapeamento dos campos e mensagens de erro.
+        problemDetail.setProperty("fields", fields);
+
+        // Deve repassar uma instância de ProblemDetail (do pacote org.springframework.http)
+        // para que a alteração tenha efeito.
+        return handleExceptionInternal(ex, problemDetail, headers, status, request);
+    }
 
     @ExceptionHandler(NegocioException.class)
     public ProblemDetail handleNegocio(NegocioException e) {
@@ -41,5 +83,14 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         problemDetail.setType(URI.create("https://github.leoarj.superatecnologia.desafio.com/erros/nao-encontrado"));
 
         return problemDetail;
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ProblemDetail handleAccessDeniedException(AccessDeniedException e) {
+        ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.FORBIDDEN); // 403
+        problem.setTitle("Acesso negado");
+        problem.setDetail(e.getMessage());
+        problem.setType(URI.create("https://github.leoarj.superatecnologia.desafio.com/erros/acesso-negado"));
+        return problem;
     }
 }
