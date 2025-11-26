dfa6c367d7cd9c0156164c905f05797e38817971
Leandro Antônio Rocha Araújo <39884377+leoarj@users.noreply.github.com>
2025-11-26 19:03:51 -0400

    feat(domain/security): Adiciona exception para exceção de token inválido

---
+++ src/main/java/com/github/leoarj/superatecnologia/desafio/domain/exception/TokenInvalidoException.java
@@ -0,0 +1,8 @@
+package com.github.leoarj.superatecnologia.desafio.domain.exception;
+
+public class TokenInvalidoException extends RuntimeException {
+
+    public TokenInvalidoException(String message) {
+        super(message);
+    }
+}
