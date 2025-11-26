fa9b4686897b33202f78784cc43a17f8b1898a3b
Leandro Antônio Rocha Araújo <39884377+leoarj@users.noreply.github.com>
2025-11-26 18:43:08 -0400

    fix(domain/repository): Corrige consulta JPQL para carregar Departamentos e Usuarios

--- src/main/java/com/github/leoarj/superatecnologia/desafio/domain/repository/SolicitacaoRepository.java
+++ src/main/java/com/github/leoarj/superatecnologia/desafio/domain/repository/SolicitacaoRepository.java
@@ -12,11 +12,11 @@ import java.util.List;
 @Repository
 public interface SolicitacaoRepository extends JpaRepository<Solicitacao, Long> {
 
-    // Usuário: Consultar apenas suas próprias solicitações"
-    // TODO: Depois externalizar (META-INF/XML) consultas JPQ grandes (+3 linhas)
     @Query("SELECT DISTINCT s FROM Solicitacao s " +
-            "LEFT JOIN FETCH s.modulosSolicitados " +
-            "WHERE s.usuario.id = :usuarioId")
-    Page<Solicitacao> findByUsuarioId(Long usuarioId, Pageable pageable);
+        "LEFT JOIN FETCH s.modulosSolicitados " +
+        "LEFT JOIN FETCH s.usuario u " +
+        "LEFT JOIN FETCH u.departamento " +
+        "WHERE s.usuario.id = :usuarioId and s.id = :id")
+    Optional<Solicitacao> findByIdComRelacionamentos(Long usuarioId, Long id);
 
 }
