09d136a2285bc8d6f4eb892be469d4b4f5b4658c
Leandro Antônio Rocha Araújo <39884377+leoarj@users.noreply.github.com>
2025-11-26 19:26:05 -0400

    feat(domain/repository): Adiciona repository para entidade Departamento

---
+++ src/main/java/com/github/leoarj/superatecnologia/desafio/domain/repository/DepartamentoRepository.java
@@ -0,0 +1,9 @@
+package com.github.leoarj.superatecnologia.desafio.domain.repository;
+
+import com.github.leoarj.superatecnologia.desafio.domain.model.Departamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartamentoRepository extends JpaRepository<Departamento, Long> {
+}
