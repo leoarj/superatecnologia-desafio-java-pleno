f392d7140f0adf70604ce714fd9ff48a48aa6ad6
Leandro Antônio Rocha Araújo <39884377+leoarj@users.noreply.github.com>
2025-11-26 18:45:47 -0400

    feat(domain/repository): Adiciona Page no SolicitacaoRepository

--- src/main/java/com/github/leoarj/superatecnologia/desafio/domain/repository/SolicitacaoRepository.java
+++ src/main/java/com/github/leoarj/superatecnologia/desafio/domain/repository/SolicitacaoRepository.java
@@ -1,16 +1,23 @@
 package com.github.leoarj.superatecnologia.desafio.domain.repository;
 
 import com.github.leoarj.superatecnologia.desafio.domain.model.Solicitacao;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface SolicitacaoRepository extends JpaRepository<Solicitacao, Long> {
+public interface SolicitacaoRepository extends JpaRepository<Solicitacao, Long>,
+        JpaSpecificationExecutor<Solicitacao> {
+
+    // Usuário: Consultar apenas suas próprias solicitações (foi passado para a specification)"
+//    // TODO: Depois externalizar (META-INF/XML) consultas JPQ grandes (+3 linhas)
+//    @Query("SELECT DISTINCT s FROM Solicitacao s " +
+//            "LEFT JOIN FETCH s.modulosSolicitados " +
+//            "WHERE s.usuario.id = :usuarioId")
+//    Page<Solicitacao> findByUsuarioId(Long usuarioId, Pageable pageable);
 
     @Query("SELECT DISTINCT s FROM Solicitacao s " +
         "LEFT JOIN FETCH s.modulosSolicitados " +
